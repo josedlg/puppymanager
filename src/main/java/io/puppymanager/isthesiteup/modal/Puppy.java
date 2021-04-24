2d13262fb24ac1fa09e3eab079787b4a6c107fbf
Jose Delaguarda <josedelaguarda@macbook-pro-2.myfiosgateway.com>
2021-04-24 13:52:40 -0400

    adding all files

---
+++ src/main/java/io/puppymanager/isthesiteup/modal/Puppy.java
@@ -0,0 +1,13 @@
+package io.puppymanager.isthesiteup.modal;
+
+import java.io.Serializable;
+
+public class Puppy implements Serializable{
+    private Long id;
+    private String bread; 
+    private String name;
+    private String email;
+    private Phone phone;
+    private String imageURL;
+
+}
