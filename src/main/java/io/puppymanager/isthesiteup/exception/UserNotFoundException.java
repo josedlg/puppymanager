d7d678e416b14c4c8f603d2854bdcb7f1e32d017
Jose Delaguarda <josedelaguarda@macbook-pro-2.myfiosgateway.com>
2021-05-06 15:53:00 -0400

    added services class and exception

---
+++ src/main/java/io/puppymanager/isthesiteup/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package io.puppymanager.isthesiteup.exception;
+
+
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message){
+        super(message);
+
+    }
+
+}
